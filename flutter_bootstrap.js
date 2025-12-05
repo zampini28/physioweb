185cdf0f11e25fb05a989c86931d2989df834e87
user <user@localhost>
2025-12-05 05:19:07 -0300

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "244637569"
+    serviceWorkerVersion: "3280911672"
   }
 });
